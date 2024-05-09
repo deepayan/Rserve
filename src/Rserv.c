62e9ad5639aa42919f65d1010999b5a977b66be5
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-05-09 18:24:01 +0530

    fix sprintf usage

--- src/Rserv.c
+++ src/Rserv.c
@@ -500,7 +500,8 @@ int compute_subprocess = 0;
 
 
 int write_debug_message(char *s) {
-	char *DBG_FILE = sprintf("%s/rserve-log.txt", "/c/Users/Deepayan Sarkar");
+	FILE *out, char DBG_FILE[1024];
+	sprintf(DBG_FILE, "%s/rserve-log.txt", "/c/Users/Deepayan Sarkar");
 	FILE *out = fopen(DBG_FILE, "a");
 	fprintf(out, s);
 	fclose(out);
